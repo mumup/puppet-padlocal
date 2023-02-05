54886c7776b09bfd7714c8c2f455ac44a5372123
mumup <pmumu142535@gmail.com>
2023-02-05 17:03:34 +0800

    chore: 代码格式

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,12 @@ const rules = {
   semi: ["error", "always"],
   quotes: ["error", "double"],
   'space-before-function-paren': ["error", "never"],
+  "@typescript-eslint/no-misused-promises": [
+    "error",
+    {
+      "checksVoidReturn": false
+    }
+  ]
 }
 
 module.exports = {
